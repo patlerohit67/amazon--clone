59d535ef7cc188e85cd10e5f94d273d50b1cfbbd
Rohit Patle <patlerohit67@gmail.com>
2023-01-14 16:23:56 +0530

    third commit

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -24,7 +24,7 @@ function Home() {
             id="49538094"
             title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
             price={239.0}
-            rating={4}
+            rating={3}
             image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
           />
         </div>
